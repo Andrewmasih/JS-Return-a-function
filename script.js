cc4221b6af3752e1b4210f91950fc873f60b8170
Andrew Masih <armasih@outlook.com>
2021-05-05 00:28:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function motivateMe () {
+  /*write the motivateMe () function here*/
+  return "A winner is just loser who tried one more time";
+
+  /* 'return' is a keyword in JS, it returns the value from the function and stops further execution, just add the keyword and the expression you want to return */
+  
+} 
+
+/*call the functon here!*/
+motivateMe();
